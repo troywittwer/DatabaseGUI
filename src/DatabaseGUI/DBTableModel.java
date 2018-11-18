cf926444c14230fa930b78d18bee12d2e907f5c3
twittwer9029 <twittwer9029@eagle.fgcu.edu>
2018-11-17 20:13:34 -0500

    Changed the TextField object to a TextArea object so that users may write their query on multiple lines.

--- src/DatabaseGUI/DBTableModel.java
+++ src/DatabaseGUI/DBTableModel.java
@@ -1,3 +1,10 @@
+/**
+ * Written by Troy Wittwer
+ *
+ * This class is largely based on "ResultSetTableModel" class from Oracle's BooksDB example
+ * project.
+ */
+
 package DatabaseGUI;
 
 import java.sql.Connection;
@@ -8,6 +15,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class DBTableModel {
+
   private final Connection connection;
   private final Statement sqlStatement;
   private ResultSet resultSet;
@@ -16,52 +24,88 @@ public class DBTableModel {
 
   private boolean databaseConnected = false;
 
+  /**
+   * Constructor for the DBTableModel class. This constructor will check to see if a connection can
+   * be made, create the connection to the database, assign a true value to the boolean representing
+   * whether or not the database has been connected to, and will pass the user's query to the
+   * setQuery method.
+   *
+   * @param url a String literal holding the URL location of the database
+   * @param userName a String literal holding the username that I created when setting up database
+   * @param pw a String literal holding the password taht I created when setting up database
+   * @param sqlQuery a String literal containing the default query.
+   * @throws SQLException database connection may result in an SQL error if the connection fails
+   */
   public DBTableModel(String url, String userName, String pw, String sqlQuery) throws SQLException {
+    // use the first three parameters in an object used to try and connect to the database
     connection = DriverManager.getConnection(url, userName, pw);
 
+    // ResultSet.TYPE_SCROLL_INSENSITIVE means:
+    //    The cursor can scroll forward and backward. Also, changes made to database aren't updated
+    //    in real time, so the results will not change while viewing.
+    // ResultSet.CONCUR_READ_ONLY means:
+    //    the information returned cannot be modified. It is read-only.
     sqlStatement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,
         ResultSet.CONCUR_READ_ONLY);
 
-    databaseConnected = true;
+    databaseConnected = true; // if this statement has been reached then the database is connected
 
+    // Pass the query over to the setQuery method.
     setQuery(sqlQuery);
-  }
+  }// end of DBTableModel constructor
 
-  public ResultSet getResultSet(){
-    return resultSet;
-  }
 
+  /**
+   * This method came from Oracle's ResultSetTableModel class. The only line I'm currently using is
+   * the only that assigns an executed query to the ResultSet object.
+   *
+   * @param sqlQuery a String object containing the user's query.
+   * @throws SQLException needed to throw exception due to the try block in the Main class.
+   */
   public void setQuery(String sqlQuery) throws SQLException {
     resultSet = sqlStatement.executeQuery(sqlQuery);
 
+    // Not being used, yet, but will provide useful info such as the amount of columns / rows
+    // created by the query. These values may change depending on the query used. At the moment,
+    // my default query only returns one column of information, so this is not necessary. If the user
+    // wanted to return all information relevant to a customer instead of just their last name, this
+    // meta data would be useful.
     metaData = resultSet.getMetaData();
 
-    resultSet.last();
-    numberOfRows = resultSet.getRow();
-  }
+    // The first command sets the ResultSet object to focus on the very last object.
+    // The second command returns the current row number. Since it's focusing on the last object,
+    // integer value returned will represent the total number of rows.
+    resultSet.last(); // Not being used, yet.
+    numberOfRows = resultSet.getRow(); // Not being used, yet.
+  }// end of setQuery method
 
-  public String getQueryAnswer(){
-    try{
+  /**
+   * As the initial project submission, this method is only returning a single column of output from
+   * the ResultSet object. For this reason, I've only typed "1" as the argument for the column
+   * index. In the next submission, this not have such a trivial argument.
+   */
+  public String getQueryAnswer() {
+    try {
       return resultSet.getString(1);
-    }
-    catch (SQLException sqlEx){
+    } catch (SQLException sqlEx) { // in case ResultSet object has trouble accessing the SQL result.
       return "Issue with database";
-    }
-  }
+    } // end of catch
+  }// end of getQueryAnswer method
 
-  public void disconnectFromDatabase(){
-    if(databaseConnected){
-      try{
-        resultSet.close();
-        sqlStatement.close();
-        connection.close();
-      }
-      catch(SQLException sqlEx){
+  /**
+   * Manually disconnects from the database and sets the databaseConnected boolean back to false.
+   */
+  public void disconnectFromDatabase() {
+    if (databaseConnected) {
+      try { // closes the three SQL objects.
+        resultSet.close();    // close result set
+        sqlStatement.close(); // close Statement object
+        connection.close();   // close database connection
+      } catch (SQLException sqlEx) { // in case one or more close operations ran into an error.
         sqlEx.printStackTrace();
-      }
-      finally{
+      } finally { // forces the class boolean value back to false in case an issue occurs during try
         databaseConnected = false;
-      }
-    }
-  }
-}
+      }// end of finally block
+    }// end of if statement
+  }// end of disconnectFromDatabase method
+}// end of DBTableModel class
--- src/DatabaseGUI/Main.java
+++ src/DatabaseGUI/Main.java
@@ -13,11 +13,12 @@ import javafx.event.ActionEvent;
 import java.sql.SQLException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.TextArea;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
@@ -36,10 +37,10 @@ public class Main extends Application implements EventHandler<ActionEvent> {
   private static final String DATABASE_URL = "jdbc:derby:lib\\salesDB";
   private static final String USERNAME = "twittwer";
   private static final String PASSWORD = "12345";
-  private static final String DEFAULT_QUERY = "SELECT lastName FROM customers WHERE customerID = 1";
+  private static final String DEFAULT_QUERY = "SELECT lastName\nFROM employees\nWHERE employeeID = 1";
 
   /**
-   * Basic components of the initial GUI turn-in. queryBox is a TextField that the user can type a
+   * Basic components of the initial GUI turn-in. queryBox is a TextArea that the user can type a
    * query into. queryButton is a Button that the user can press to run the query. queryAnswer is a
    * Label that displays the result of the query. To keep it simple, this label only displays one
    * column of the output.
@@ -48,7 +49,7 @@ public class Main extends Application implements EventHandler<ActionEvent> {
    */
   private static DBTableModel myTable;
   private static Button queryButton;
-  private static TextField queryBox;
+  private static TextArea queryBox;
   private static Label queryAnswer;
 
   @Override
@@ -68,19 +69,26 @@ public class Main extends Application implements EventHandler<ActionEvent> {
       myGrid.setVgap(10); // vertical gap of 10 pixels between components
       myGrid.setHgap(10); // height gap of 10 pixels between components
 
-      queryBox = new TextField(DEFAULT_QUERY); // populate the text field with the default query
+      Label testLabel = new Label("Last name: ");
+      queryBox = new TextArea(DEFAULT_QUERY); // populate the text field with the default query
       queryButton = new Button("Select Query"); // Button's text reads as "Select Query"
       queryAnswer = new Label("I'm the default text"); // Couldn't tell where the label was.
+
+      queryBox.setPrefWidth(400);
+      queryBox.setPrefHeight(100);
+      //queryBox
+      //queryBox.setAlignment(Pos.TOP_LEFT);
 
       // add the three components to the GridPane object.
       myGrid.add(queryBox, 0, 0, 1, 1);
       myGrid.add(queryButton, 1, 0, 1, 1);
-      myGrid.add(queryAnswer, 0, 1, 1, 1);
+      myGrid.add(testLabel, 0, 1, 1, 1);
+      myGrid.add(queryAnswer, 1, 1, 1, 1);
 
       queryButton.setOnAction(this); // leads to the handle method. Defines what button does.
 
       primaryStage.setTitle("Initial GUI Project");
-      primaryStage.setScene(new Scene(myGrid, 300, 275)); // add GridPane to scene
+      primaryStage.setScene(new Scene(myGrid, 800, 350)); // add GridPane to scene
       primaryStage.show();
 
       // Need to find the JavaFX equivalent to TableRowSorter & TableModel.
@@ -111,7 +119,7 @@ public class Main extends Application implements EventHandler<ActionEvent> {
       } catch (SQLException sqlEx) { // myTable may result in an SQLException
         System.out.println("Issue with SQL statement");
 
-        try { // sets the TextField back to default text
+        try { // sets the TextArea back to default text
           myTable.setQuery(DEFAULT_QUERY);
           queryBox.setText(DEFAULT_QUERY);
         } catch (SQLException sqlEx2) { // again, myTable may result in an SQLException
@@ -128,5 +136,5 @@ public class Main extends Application implements EventHandler<ActionEvent> {
    */
   public static void main(String[] args) {
     launch(args);
-  }
-}
+  }// end of main method
+}// end of Main class
